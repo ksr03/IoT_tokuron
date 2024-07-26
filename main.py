ff3d6f53f40a1a81c12e14ef0cafbf348c4af05c
ksr03 <K_Haruka03@outlook.jp>
2024-07-26 18:03:24 +0900

    line botでメッセージを送れるように設定

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import paho.mqtt.client as mqtt
+from paho.mqtt import client as mqtt_client
 from flask import Flask
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage
@@ -13,37 +13,51 @@ app = Flask(__name__)
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 USER_ID = os.getenv('USER_ID')
-MQTT_TOPIC = os.getenv('MQTT_TOPIC')
+MQTT_TOPIC_TEMP = os.getenv('MQTT_TOPIC_TEMP')
+MQTT_TOPIC_HUMID = os.getenv('MQTT_TOPIC_HUMID')
 
-# Initialize LINE bot API and handler
-# line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(LINE_CHANNEL_SECRET)
+# LINE Botの設定
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# MQTT callbacks
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe(MQTT_TOPIC)
+# MQTTブローカーへの接続
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print(f"Failed to connect, return code {rc}")
 
-def on_message(client, userdata, msg):
-    message = msg.payload.decode()
-    print(f"Received message: {message} on topic {msg.topic}")
-    # line_bot_api.push_message(USER_ID, TextSendMessage(text=message))
-    print('Message sent to LINE')
+    client = mqtt_client.Client()
+    client.on_connect = on_connect
+    client.connect('broker.hivemq.com', 1883)
+    return client
 
-# MQTTクライアントの作成
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
+# MQTTトピックの購読
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        if msg.topic == MQTT_TOPIC_TEMP:
+            text = f"今の気温は {msg.payload.decode()} 度です"
+        elif msg.topic == MQTT_TOPIC_HUMID:
+            text = f"今の湿度は {msg.payload.decode()} %です"
 
-# Connect to MQTT broker
-mqtt_client.connect('broker.hivemq.com', 1883, 60)
+        line_bot_api.push_message(USER_ID, TextSendMessage(text=text))
+        print('Message sent to LINE')
+    
+    client.subscribe([(MQTT_TOPIC_TEMP, 0), (MQTT_TOPIC_HUMID, 0)])
+    client.on_message = on_message
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
 
 @app.route('/')
 def home():
-    return MQTT_TOPIC
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     # Start Flask app
-    app.run(port=3000)
+    # app.run(port=3000)
     # Start MQTT loop
-    mqtt_client.loop_start()
+    run()
